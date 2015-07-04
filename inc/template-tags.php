17a151d677e465a6f15c5ee62d8af7d2d33e25ab
leac <leac@ort.org.il>
2015-07-05 00:45:36 +0300

    Pagination - Use core pagination if plugin doesn't exist. Fixes #10

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -6,6 +6,67 @@
  *
  * @package kamoha
  */
+if ( !function_exists( 'kamoha_paging_nav' ) ) :
+
+    /**
+     * Display navigation to next/previous set of posts when applicable.
+     *
+     * @return void
+     */
+    function kamoha_paging_nav(){
+        // Don't print empty markup if there's only one page.
+        if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+            return;
+        }
+        ?>
+        <nav class="navigation paging-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'kamoha' ); ?></h1>
+            <div class="nav-links">
+
+                <?php if ( get_next_posts_link() ) : ?>
+                    <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'kamoha' ) ); ?></div>
+                <?php endif; ?>
+
+                <?php if ( get_previous_posts_link() ) : ?>
+                    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'kamoha' ) ); ?></div>
+                <?php endif; ?>
+
+            </div><!-- .nav-links -->
+        </nav><!-- .navigation -->
+        <?php
+    }
+
+endif;
+
+if ( !function_exists( 'kamoha_post_nav' ) ) :
+
+    /**
+     * Display navigation to next/previous post when applicable.
+     *
+     * @return void
+     */
+    function kamoha_post_nav(){
+        // Don't print empty markup if there's nowhere to navigate.
+        $previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+        $next = get_adjacent_post( false, '', false );
+
+        if ( !$next && !$previous ) {
+            return;
+        }
+        ?>
+        <nav class="navigation post-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'kamoha' ); ?></h1>
+            <div class="nav-links">
+                <?php
+                previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'kamoha' ) );
+                next_post_link( '<div class="nav-next">%link</div>', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link', 'kamoha' ) );
+                ?>
+            </div><!-- .nav-links -->
+        </nav><!-- .navigation -->
+        <?php
+    }
+
+endif;
 
 if ( !function_exists( 'kamoha_posted_on' ) ) :
 
