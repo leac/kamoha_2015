48339a3ca7c69ee36eb518e02566c0726839d598
Lea Cohen <leac@ort.org.il>
2015-08-25 22:47:48 +0300

    Remove favicon tag from header, as WP 4.3 enables uploading it in the customizer. Fixes #4.

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/favicon.ico" />
         <?php
         // For facebook link details
         ?>
