1655ade2acae69e7b74c42328d8de9c73350bf0a
Lea Cohen <leac@ort.org.il>
2015-12-24 03:21:45 +0200

    masonry table of contents - add hover link underline

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ function kamoha_scripts() {
         // Style.css belongs everywhere execept in the site documentation page template:
         if ( !is_page_template( 'page_site_documentation.php' ) ) {
 
-            wp_enqueue_style( 'kamoha-style', get_stylesheet_uri(), array(), '1.6.2.5' );
+            wp_enqueue_style( 'kamoha-style', get_stylesheet_uri(), array(), '1.6.2.6' );
 
             // The following scripts and styles belong on every regular site page, except the 2 page templates
             if ( !is_page_template( 'page_without-header-and-footer.php' ) ) {
@@ -115,7 +115,7 @@ function kamoha_scripts() {
 
                 wp_enqueue_script( 'kamoha-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-                wp_enqueue_script( 'kamoha-script', get_template_directory_uri() . '/js/script.js', array('jquery'), 1, TRUE );
+                wp_enqueue_script( 'kamoha-script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.1', TRUE );
 
                 if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
                     wp_enqueue_script( 'comment-reply' );
