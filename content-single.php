ab43d7ba49941be9aef683eac522c5ec0009503c
Lea Cohen <leac@ort.org.il>
2016-01-27 03:28:40 +0200

    Fixed:  Missing: wp_link_pages() in content-single.php. Fixes #26

--- content-single.php
+++ content-single.php
@@ -20,39 +20,45 @@
 
     <?php
     /* If there is an excerpt, show it. Don't show auto-generated excerpts. Check if the user has written an excerpt with has_excerpt()  */
-    if (has_excerpt()) {
+    if ( has_excerpt() ) {
         ?>
         <div class="entry-summary">
-        <?php the_excerpt(); ?>
+            <?php the_excerpt(); ?>
         </div><!-- .entry-summary -->
-<?php } ?>
+    <?php } ?>
 
     <div class="entry-content">
-<?php the_content(); ?>
+        <?php the_content(); ?>
+        <?php
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . __( 'Pages:', 'kamoha' ),
+            'after' => '</div>',
+        ) );
+        ?>
     </div><!-- .entry-content -->
 
-<?php // in the footer, show tags, and share buttons   ?>
+    <?php // in the footer, show tags, and share buttons   ?>
     <footer class="entry-meta clear">
 
         <div class="entry-tags">
             <?php
             /* translators: used between list items, there is a space after the comma */
-            $tag_list = get_the_tag_list('', __(', ', 'kamoha'));
+            $tag_list = get_the_tag_list( '', __( ', ', 'kamoha' ) );
 
-            if ('' != $tag_list) {
+            if ( '' != $tag_list ) {
                 printf(
-                        __('This entry was tagged %1$s.', 'kamoha'), $tag_list
+                        __( 'This entry was tagged %1$s.', 'kamoha' ), $tag_list
                 );
             }
             ?>
 
-<?php edit_post_link(__('Edit', 'kamoha'), '<span class="edit-link">', '</span>'); ?>
+            <?php edit_post_link( __( 'Edit', 'kamoha' ), '<span class="edit-link">', '</span>' ); ?>
         </div>
 
         <?php
         //Show Simple share buttons widget: 
-        if (function_exists('ssba_buttons')) { // run the shortcode only if the function associated with it exists
-            echo do_shortcode('[ssba]');
+        if ( function_exists( 'ssba_buttons' ) ) { // run the shortcode only if the function associated with it exists
+            echo do_shortcode( '[ssba]' );
         }
         ?> 
 
@@ -62,7 +68,7 @@
 
 <?php // after post, show related articles ?>
 <?php
-if (function_exists('wp_related_posts')) {
+if ( function_exists( 'wp_related_posts' ) ) {
     wp_related_posts();
 }
 
