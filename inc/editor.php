b3d0f4de092d23814b7d4e440a1c4a60edb7fd5a
Lea Cohen <leac@ort.org.il>
2016-02-20 22:51:47 +0200

    tinymce style formats to plugin

--- inc/editor.php
+++ inc/editor.php
@@ -52,39 +52,3 @@ function kamoha_remove_normal_excerpt() { /* this added on my own */
 add_action( 'admin_menu', 'kamoha_remove_normal_excerpt' );
 
 
-
-/* -----------------------------------------
- * Add style select to visual editor
- * ----------------------------------------- */
-
-// Callback function to insert 'styleselect' into the $buttons array
-function kamoha_mce_buttons_2( $buttons ) {
-    array_unshift( $buttons, 'styleselect' );
-    return $buttons;
-}
-
-// Register our callback to the appropriate filter
-add_filter( 'mce_buttons_2', 'kamoha_mce_buttons_2' );
-
-
-// Callback function to filter the MCE settings
-function kamoha_mce_before_init_insert_formats( $init_array ) {  
-	// Define the style_formats array
-	$style_formats = array(  
-		// Each array child is a format with it's own settings
-		array(  
-			'title' => __('.letter', 'kamoha'),  
-			'block' => 'div',  
-			'classes' => 'letter',
-			'wrapper' => true,
-			
-		),  
-	);  
-	// Insert the array, JSON ENCODED, into 'style_formats'
-	$init_array['style_formats'] = json_encode( $style_formats );  
-	
-	return $init_array;  
-  
-} 
-// Attach callback to 'tiny_mce_before_init' 
-add_filter( 'tiny_mce_before_init', 'kamoha_mce_before_init_insert_formats' );  
