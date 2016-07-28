54421082205b43071d42cdf19800f635428b5a52
Lea Cohen <leac@ort.org.il>
2016-07-28 15:14:23 +0300

    fix content width to 800

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-    $content_width = 640; /* pixels */
+    $content_width = 800; /* pixels */
 }
 
 if ( ! function_exists( 'kamoha_setup' ) ) :
